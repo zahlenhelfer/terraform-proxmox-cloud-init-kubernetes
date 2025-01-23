309a01ce0bea21a27141784e5b5498a25e74c0b1
Marcus Ross <marcus.ross@zahlenhelfer.de>
2025-01-23 14:09:16 +0100

    Refactor control plane VM output to use variable indexing for IPv4 addresses and remove unused resources

--- controlplane_vm_after_creation.tf
+++ controlplane_vm_after_creation.tf
@@ -10,24 +10,12 @@
 #Step 1: write ip for backup reasons to disk (required for step 3 and 4)
 output "ctrl_ipv4_address" {
   depends_on = [proxmox_virtual_environment_vm.k8s-ctrl]
-  value      = proxmox_virtual_environment_vm.k8s-ctrl["cp-0"].ipv4_addresses[1][0]
+  value      = proxmox_virtual_environment_vm.k8s-ctrl[var.controlplanes[0].name].ipv4_addresses[1][0]
 }
 
 resource "local_file" "ctrl-ip" {
-  content         = proxmox_virtual_environment_vm.k8s-ctrl["cp-0"].ipv4_addresses[1][0]
+  content         = proxmox_virtual_environment_vm.k8s-ctrl[var.controlplanes[0].name].ipv4_addresses[1][0]
   filename        = "output/ctrl-ip.txt"
-  file_permission = "0644"
-}
-
-resource "local_file" "ctrl-var" {
-  content         = var.controlplanes[0].name
-  filename        = "output/dbg-var-name.txt"
-  file_permission = "0644"
-}
-
-resource "local_file" "dbg-ip" {
-  content         = jsonencode(proxmox_virtual_environment_vm.k8s-ctrl)
-  filename        = "output/dbg-ctrl-ip.txt"
   file_permission = "0644"
 }
 
