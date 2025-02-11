c56c62c54b96e0c77cfa45c57afbc9816b6cec64
deB4SH <github@b4sh.de>
2025-02-11 16:37:51 +0100

    fix: hostname

--- kubernetes_controlplane_cloudinit.tf
+++ kubernetes_controlplane_cloudinit.tf
@@ -7,7 +7,7 @@ resource "proxmox_virtual_environment_file" "cloud-init-kubernetes-controlplane"
   source_raw {
     data = templatefile("${path.module}/templates/kubernetes_controlplane.yaml.tftpl", {
       #defaults
-      hostname      = var.postgres_vm_name
+      hostname      = each.value.name
       user          = var.vm_user
       user_password = var.vm_user_password
       user_pub_key  = var.vm_user_public_key
