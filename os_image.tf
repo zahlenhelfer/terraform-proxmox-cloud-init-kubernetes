743c7fec5f2ad89814d3ed46fbd40e7b926ff688
Marcus Ross <marcus.ross@zahlenhelfer.de>
2025-05-10 13:19:32 +0200

    refactor: simplify lifecycle prevent_destroy logic in OS image resource

--- os_image.tf
+++ os_image.tf
@@ -1,10 +1,19 @@
-locals {
-  pull_always = var.always_pull_os_images
+#image from here: https://cloud.debian.org/images/cloud/bookworm/daily/20240412-1715/
+resource "proxmox_virtual_environment_download_file" "image_always_pull" {
+  count    = var.always_pull_os_images ? 1 : 0
+  for_each = { for each in var.os_images : each.name => each }
+
+  node_name          = var.pve_default_node
+  content_type       = "iso"
+  datastore_id       = coalesce(var.os_images_datastore_id, var.pve_default_datastore_id)
+  file_name          = each.value.filename
+  url                = each.value.url
+  checksum           = each.value.checksum
+  checksum_algorithm = each.value.checksum_algorithm
 }
 
-#image from here: https://cloud.debian.org/images/cloud/bookworm/daily/20240412-1715/
 resource "proxmox_virtual_environment_download_file" "image" {
-
+  count    = var.always_pull_os_images ? 0 : 1
   for_each = { for each in var.os_images : each.name => each }
 
   node_name          = var.pve_default_node
@@ -16,6 +25,6 @@ resource "proxmox_virtual_environment_download_file" "image" {
   checksum_algorithm = each.value.checksum_algorithm
 
   lifecycle {
-    prevent_destroy = local.pull_always
+    prevent_destroy = true
   }
 }
